e44467a1284cb46a272c054490e3fabfdeb3fb38
Sigurd Susag <se.roope@gmail.com>
2019-11-19 12:28:18 -0700

    Create firebase_script.js

---
+++ firebase_script.js
@@ -0,0 +1,23 @@
+  <!-- The core Firebase JS SDK is always required and must be listed first -->
+  <script src="https://www.gstatic.com/firebasejs/7.4.0/firebase-app.js"></script>
+
+  <!-- TODO: Add SDKs for Firebase products that you want to use
+       https://firebase.google.com/docs/web/setup#available-libraries -->
+  <script src="https://www.gstatic.com/firebasejs/7.4.0/firebase-analytics.js"></script>
+
+  <script>
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+      apiKey: "AIzaSyC1Jhri3sVPVIWB9_BgPiwHi5PdwUBbU3E",
+      authDomain: "harvest-statistics.firebaseapp.com",
+      databaseURL: "https://harvest-statistics.firebaseio.com",
+      projectId: "harvest-statistics",
+      storageBucket: "harvest-statistics.appspot.com",
+      messagingSenderId: "127478263531",
+      appId: "1:127478263531:web:6e9d97de52edca0ff4d8ae",
+      measurementId: "G-50L22X38FV"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  </script>
